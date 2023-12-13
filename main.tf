647b9292cb930efb29b48a74d0af87cb444d606a
Dmytro Oliinyk <devdm.dp@gmail.com>
2023-12-13 20:43:06 +0000

    Some edit

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
+provider "google" {
+  project = var.GOOGLE_PROJECT
+}
+
 module "gke_cluster" {
-  source         = "github.com/dm-ol/tf-google-gke-cluster"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+  source           = "github.com/dm-ol/tf-google-gke-cluster"
+  GOOGLE_REGION    = var.GOOGLE_REGION
+  GOOGLE_PROJECT   = var.GOOGLE_PROJECT
   GKE_MACHINE_TYPE = var.GKE_MACHINE_TYPE
-  GKE_NUM_NODES  = var.GKE_NUM_NODES
+  GKE_NUM_NODES    = var.GKE_NUM_NODES
 }
--- vars.tfvars
+++ vars.tfvars
@@ -1,4 +1,4 @@
-GOOGLE_REGION  = "us-central1-b"
-GOOGLE_PROJECT = "My-test"
+GOOGLE_REGION    = "us-central1-b"
+GOOGLE_PROJECT   = "My-test"
 GKE_MACHINE_TYPE = "e2-standard-2"
-GKE_NUM_NODES  = 2
+GKE_NUM_NODES    = 2
