993a878fbb08f4ad642602ec707123b241e3b689
Dmytro <149588780+dm-ol@users.noreply.github.com>
2023-12-13 14:24:53 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,23 @@
+variable "GOOGLE_PROJECT" {
+  type        = string
+  default     = "My-test"
+  description = "GCP project name"
+}
 
+variable "GOOGLE_REGION" {
+  type        = string
+  default     = "us-central1-b"
+  description = "GCP region to use"
+}
+
+variable "GKE_MACHINE_TYPE" {
+  type        = string
+  default     = "e2-standard-2"
+  description = "Machine type"
+}
+
+variable "GKE_NUM_NODES" {
+  type        = number
+  default     = 2
+  description = "GKE nodes number"
+}
